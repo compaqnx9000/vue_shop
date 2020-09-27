ea75bee3b26aae4f9f3c92f36eab7f4f27dd0b6a
飞满天 <16976440@qq.com>
2020-09-27 19:29:56 +0800

    完成登录

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-button type="info" @click="btnLogout">退出</el-button>
+</template>
+<script>
+export default {
+  data () {
+  },
+  methods: {
+    btnLogout () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/css/global.css'
+import less from 'less'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:7000/api/private/v1/'
+Vue.prototype.$http = axios
+Vue.use(less)
 
 Vue.config.productionTip = false
 
